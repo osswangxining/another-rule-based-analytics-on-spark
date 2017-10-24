98ccbd9de4b64f7dfacc4fac858ca3e090ff5c75
osswangxining <osswangxining@163.com>
2017-10-24 15:11:43 +0800

    Rule Updater

--- src/main/java/cloud/iot/ai/analytics/kafka/RuleUpdater.java
+++ src/main/java/cloud/iot/ai/analytics/kafka/RuleUpdater.java
@@ -1,15 +1,8 @@
 package cloud.iot.ai.analytics.kafka;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import cloud.iot.ai.analytics.element.Schema;
-import cloud.iot.ai.analytics.element.SchematronReader;
-import cloud.iot.ai.analytics.factory.SchematronFactory;
 
 public class RuleUpdater implements Serializable {
 
@@ -18,9 +11,9 @@ public class RuleUpdater implements Serializable {
 	 */
 	private static final long serialVersionUID = 6876708192874901880L;
 
-	private static Map<String, Schema> rules = new ConcurrentHashMap<String, Schema>();
+	private static Map<String, String> rules = new ConcurrentHashMap<String, String>();
 
-	public static Schema getRule(String id) {
+	public static String getRule(String id) {
 		return rules.get(id);
 	}
 
@@ -30,16 +23,7 @@ public class RuleUpdater implements Serializable {
 		if (id == null || rule == null) {
 			return;
 		}
-		SchematronReader reader = SchematronFactory.INSTANCE.newSchematronReader();
-
-		try {
-			Schema schema = reader.readSchematron(null, new ByteArrayInputStream(rule.getBytes("UTF-8")));
-			rules.put(id, schema);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		rules.put(id, rule);
 
 	}
 }
