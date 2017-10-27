d81e1c1e7380a37907d0ce639f983438c42b6127
osswangxining <osswangxining@163.com>
2017-10-27 16:22:13 +0800

    change to kafka010

--- src/main/java/cloud/iot/ai/analytics/streaming/AnalyticsEngine.java
+++ src/main/java/cloud/iot/ai/analytics/streaming/AnalyticsEngine.java
@@ -8,8 +8,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Durations;
@@ -17,9 +19,13 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.HasOffsetRanges;
-import org.apache.spark.streaming.kafka.KafkaUtils;
-import org.apache.spark.streaming.kafka.OffsetRange;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.ConsumerStrategy;
+import org.apache.spark.streaming.kafka010.HasOffsetRanges;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.spark.streaming.kafka010.OffsetRange;
+import org.apache.spark.streaming.kafka010.CanCommitOffsets;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -49,22 +55,108 @@ public class AnalyticsEngine {
 
 		// Create context with 2 second batch interval
 		SparkConf sparkConf = new SparkConf().setAppName("AnotherAnalyticsEngineUsingSpark").setMaster("local[2]");
+		sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(1));
 
-		HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
-		HashMap<String, String> kafkaParams = new HashMap<String, String>();
+		// HashSet<String> topicsSet = new
+		// HashSet<String>(Arrays.asList(topics.split(",")));
+		Map<String, Object> kafkaParams = new HashMap<String, Object>();
 		kafkaParams.put("metadata.broker.list", brokers);
+		kafkaParams.put("bootstrap.servers", brokers);
 		kafkaParams.put("spark.streaming.kafka.maxRatePerPartition", "100");
+		kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaParams.put("group.id", "MyAnalyticsEngineConsumerGroup1");
+		kafkaParams.put("enable.auto.commit", false);
 
 		System.out.println("initiate config....");
 		HashSet<String> configTopicSet = new HashSet<String>(Arrays.asList("metadata_rule".split(",")));
 		processRuleUpdate(jssc, brokers, configTopicSet, engineManager);
 		System.out.println("waiting for config update....");
 
+		final JsonParser parser = new JsonParser();
+
 		System.out.println("initiate messages....");
 		// Create direct kafka stream with brokers and topics
-		JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(jssc, String.class, String.class,
-				StringDecoder.class, StringDecoder.class, kafkaParams, topicsSet);
+		ConsumerStrategy<String, String> consumerStrategy = ConsumerStrategies
+				.Subscribe(Arrays.asList(topics.split(",")), kafkaParams);
+		JavaInputDStream<ConsumerRecord<String, String>> streams = KafkaUtils.createDirectStream(jssc,
+				LocationStrategies.PreferConsistent(), consumerStrategy);
+		// streams.print();
+		// streams.foreachRDD(new VoidFunction<JavaRDD<ConsumerRecord<String,
+		// String>>>() {
+		//
+		// @Override
+		// public void call(JavaRDD<ConsumerRecord<String, String>> t) throws Exception
+		// {
+		// //OffsetRange[] offsetRanges = ((HasOffsetRanges) t.rdd()).offsetRanges();
+		//
+		// List<ConsumerRecord<String, String>> records = t.collect();
+		// records.forEach(consumerRecord -> {
+		// String key = consumerRecord.key();
+		// long offset = consumerRecord.offset();
+		// int partition = consumerRecord.partition();
+		// String topic = consumerRecord.topic();
+		// String value = consumerRecord.value();
+		// //System.out.println("consumerRecord:" + consumerRecord.toString());
+		// String condition = engineManager.getRule("device001");
+		// System.out.println("condition:" + condition);
+		// JsonObject conditionJO = (JsonObject) parser.parse(condition);
+		// boolean valid = Validator.getInstance().execute(value,
+		// conditionJO.getAsJsonPrimitive("condition").getAsString());
+		// if (valid) {
+		// JsonObject event = (JsonObject) parser.parse(value);
+		// long time2 = new Date().getTime() / 1000;
+		// String key1 = "" + time2;
+		// long gap = time2 -
+		// event.getAsJsonObject("d").getAsJsonPrimitive("timestamp").getAsLong();
+		// String message = "report[" + gap + "]:" + value;
+		// KeyedMessage<String, String> keyedMessage = new KeyedMessage<String,
+		// String>("rule.output", key1,
+		// message);
+		// KafkaProducer.getInstance(brokers).getProducer().send(keyedMessage);
+		// }
+		// });
+		//
+		// // some time later, after outputs have completed
+		// //((CanCommitOffsets) streams.inputDStream()).commitAsync(offsetRanges);
+		// }
+		//
+		// });
+		streams.foreachRDD(rdd -> {
+			OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+			rdd.collect().forEach(consumerRecord -> {
+				String key = consumerRecord.key();
+				long offset = consumerRecord.offset();
+				int partition = consumerRecord.partition();
+				String topic = consumerRecord.topic();
+				String value = consumerRecord.value();
+				System.out.println("consumerRecord:" + consumerRecord.toString());
+				String condition = engineManager.getRule("device001");
+				System.out.println("condition:" + condition);
+				if (condition != null) {
+					JsonObject conditionJO = (JsonObject) parser.parse(condition);
+					boolean valid = Validator.getInstance().execute(value,
+							conditionJO.getAsJsonPrimitive("condition").getAsString());
+					if (valid) {
+						JsonObject event = (JsonObject) parser.parse(value);
+						long time2 = new Date().getTime() / 1000;
+						String key1 = "" + time2;
+						long gap = time2 - event.getAsJsonObject("d").getAsJsonPrimitive("timestamp").getAsLong();
+						String message = "report[" + gap + "]:" + value;
+						KeyedMessage<String, String> keyedMessage = new KeyedMessage<String, String>("rule.output",
+								key1, message);
+						KafkaProducer.getInstance(brokers).getProducer().send(keyedMessage);
+					}
+				}
+			});
+
+			// some time later, after outputs have completed
+			((CanCommitOffsets) streams.inputDStream()).commitAsync(offsetRanges);
+		});
+		// JavaPairInputDStream<String, String> messages =
+		// KafkaUtils.createDirectStream(jssc, String.class, String.class,
+		// StringDecoder.class, StringDecoder.class, kafkaParams, topicsSet);
 
 		// Map<TopicAndPartition, Long> topicAndPartition = new HashMap<>();
 		// topicAndPartition.put(new TopicAndPartition("test-topic", 0), 1L);
@@ -90,60 +182,64 @@ public class AnalyticsEngine {
 		// topicAndPartition, new MessageAndMetadataFunction());
 		System.out.println("Listening kafka messages....");
 		// Get the data
-		final JsonParser parser = new JsonParser();
-		messages.foreachRDD(rdd -> {
-			OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
-			Arrays.asList(offsets).forEach(offset -> {
-				String topic = offset.topic();
-				int partition = offset.partition();
-				long fromOffset = offset.fromOffset();
-				long untilOffset = offset.untilOffset();
-				System.out.println("topic:" + topic + ",partition:" + partition + ",fromOffset:" + fromOffset
-						+ ",untilOffset:" + untilOffset);
-			});
-			rdd.collect().forEach(element -> System.out.println(element));
-			// JavaPairRDD<List<String>, Long> partitions = rdd.glom().zipWithIndex();
-			// partitions.foreach(partition -> {
-			// List<String> consumerRecord = partition._1();
-			// Long index = partition._2();
-			// System.out.println("index:" + index);
-			// consumerRecord.forEach(record -> {
-			//// String _1 = record._1();
-			//// String _2 = record._2();
-			//// System.out.println("_1:" + _1 + ", _2:" + _2);
-			// System.out.println("record:" + record);
-			// });
-			// });
-		});
-		messages.foreachRDD(new VoidFunction<JavaPairRDD<String, String>>() {
 
-			@Override
-			public void call(JavaPairRDD<String, String> v1) throws Exception {
-				Map<String, String> events = v1.collectAsMap();
-
-				System.out.println("New RDD call, size=" + events.size());
-
-				events.forEach((k, v) -> {
-					String condition = engineManager.getRule("device001");
-					System.out.println("condition:" + condition);
-					JsonObject conditionJO = (JsonObject) parser.parse(condition);
-					boolean valid = Validator.getInstance().execute(v, conditionJO.getAsJsonPrimitive("condition").getAsString());
-					if (valid) {
-						System.out.println(k + ":" + v);
-						JsonObject event = (JsonObject) parser.parse(v);
-						long time2 = new Date().getTime() / 1000;
-						String key = "" + time2;
-						long gap = time2 - event.getAsJsonObject("d").getAsJsonPrimitive("timestamp").getAsLong();
-						String message = "report[" + gap + "]:" + v;
-						String topic = "rule.output";
-						KeyedMessage<String, String> keyedMessage = new KeyedMessage<String, String>(topic, key,
-								message);
-						KafkaProducer.getInstance(brokers).getProducer().send(keyedMessage);
-					}
-				});
-
-			}
-		});
+		// messages.foreachRDD(rdd -> {
+		// OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+		// Arrays.asList(offsets).forEach(offset -> {
+		// String topic = offset.topic();
+		// int partition = offset.partition();
+		// long fromOffset = offset.fromOffset();
+		// long untilOffset = offset.untilOffset();
+		// System.out.println("topic:" + topic + ",partition:" + partition +
+		// ",fromOffset:" + fromOffset
+		// + ",untilOffset:" + untilOffset);
+		// });
+		// rdd.collect().forEach(element -> System.out.println(element));
+		// // JavaPairRDD<List<String>, Long> partitions = rdd.glom().zipWithIndex();
+		// // partitions.foreach(partition -> {
+		// // List<String> consumerRecord = partition._1();
+		// // Long index = partition._2();
+		// // System.out.println("index:" + index);
+		// // consumerRecord.forEach(record -> {
+		// //// String _1 = record._1();
+		// //// String _2 = record._2();
+		// //// System.out.println("_1:" + _1 + ", _2:" + _2);
+		// // System.out.println("record:" + record);
+		// // });
+		// // });
+		// });
+		// messages.foreachRDD(new VoidFunction<JavaPairRDD<String, String>>() {
+		//
+		// @Override
+		// public void call(JavaPairRDD<String, String> v1) throws Exception {
+		// Map<String, String> events = v1.collectAsMap();
+		//
+		// System.out.println("New RDD call, size=" + events.size());
+		//
+		// events.forEach((k, v) -> {
+		// String condition = engineManager.getRule("device001");
+		// System.out.println("condition:" + condition);
+		// JsonObject conditionJO = (JsonObject) parser.parse(condition);
+		// boolean valid = Validator.getInstance().execute(v,
+		// conditionJO.getAsJsonPrimitive("condition").getAsString());
+		// if (valid) {
+		// System.out.println(k + ":" + v);
+		// JsonObject event = (JsonObject) parser.parse(v);
+		// long time2 = new Date().getTime() / 1000;
+		// String key = "" + time2;
+		// long gap = time2 -
+		// event.getAsJsonObject("d").getAsJsonPrimitive("timestamp").getAsLong();
+		// String message = "report[" + gap + "]:" + v;
+		// String topic = "rule.output";
+		// KeyedMessage<String, String> keyedMessage = new KeyedMessage<String,
+		// String>(topic, key,
+		// message);
+		// KafkaProducer.getInstance(brokers).getProducer().send(keyedMessage);
+		// }
+		// });
+		//
+		// }
+		// });
 
 		// JavaDStream<String> data = messages.map(new Function<Tuple2<String, String>,
 		// String>() {
@@ -233,31 +329,43 @@ public class AnalyticsEngine {
 
 	private static void processRuleUpdate(JavaStreamingContext jssc, String brokers, Set<String> topicsSet,
 			final AnalyticsEngineManager engineManager) {
-		HashMap<String, String> kafkaParams = new HashMap<String, String>();
+		Map<String, Object> kafkaParams = new HashMap<String, Object>();
 		kafkaParams.put("metadata.broker.list", brokers);
-		kafkaParams.put("auto.offset.reset", "smallest");
+		kafkaParams.put("bootstrap.servers", brokers);
+		kafkaParams.put("spark.streaming.kafka.maxRatePerPartition", "100");
+		kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaParams.put("group.id", "MyAnalyticsEngineConsumerGroup1");
+		kafkaParams.put("enable.auto.commit", false);
+		kafkaParams.put("auto.offset.reset", "earliest");
 
 		System.out.println("Initiate kafka messages for rules....");
 		// Create direct kafka stream with brokers and topics
-		JavaPairInputDStream<String, String> rules = KafkaUtils.createDirectStream(jssc, String.class, String.class,
-				StringDecoder.class, StringDecoder.class, kafkaParams, topicsSet);
+		ConsumerStrategy<String, String> consumerStrategy = ConsumerStrategies.Subscribe(topicsSet, kafkaParams);
+		JavaInputDStream<ConsumerRecord<String, String>> streams = KafkaUtils.createDirectStream(jssc,
+				LocationStrategies.PreferConsistent(), consumerStrategy);
+
 		System.out.println("Waiting for kafka messages of rules....");
 
 		// Get the data
-		JavaDStream<String> data = rules.map(new Function<Tuple2<String, String>, String>() {
-			@Override
-			public String call(Tuple2<String, String> tuple2) {
-				String key = tuple2._1();
-				String value = tuple2._2();
+		streams.foreachRDD(rdd -> {
+			rdd.collect().forEach(consumerRecord -> {
+				String key = consumerRecord.key();
+				long offset = consumerRecord.offset();
+				int partition = consumerRecord.partition();
+				String topic = consumerRecord.topic();
+				String value = consumerRecord.value();
+				System.out.println("consumerRecord:" + consumerRecord.toString());
 				System.out.println("[ruleupdate]key:" + key + ", value:" + value);
 
 				engineManager.getEngine().addRule(key, value);
+			});
 
-				return key;
-			}
+			OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+			// some time later, after outputs have completed
+			((CanCommitOffsets) streams.inputDStream()).commitAsync(offsetRanges);
 		});
 
-		data.print();
 		System.out.println("Prepare rule validation....");
 
 	}
